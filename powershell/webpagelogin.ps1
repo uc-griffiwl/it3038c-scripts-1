2dc4b3a0c690ad4ea7e453d98f79bdbfe2ac8bf1
uc-watkinwj <watkinwj@mail.uc.edu>
2019-09-27 19:57:35 -0400

    adding project file1

---
+++ powershell/webpagelogin.ps1
@@ -0,0 +1,8 @@
+﻿$navOpenInBackgroundTab = 0x1000;
+$ie = new-object -com InternetExplorer.Application
+$ie.Navigate2("http://canopy.uc.edu/webapps/login/?action=relogin");
+$ie.Navigate2("https://www.twitch.tv", $navOpenInBackgroundTab);
+$ie.Navigate2("https://www.reddit.com/r/legaladvice/ ", $navOpenInBackgroundTab);
+$ie.Navigate2("https://www.reddit.com/r/nfl/ ", $navOpenInBackgroundTab);
+$ie.Navigate2("https://www.reddit.com/r/fantasyfootball/ ", $navOpenInBackgroundTab);
+$ie.Visible = $true;
