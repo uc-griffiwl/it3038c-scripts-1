edb2fa265d25aab6d62d5e7b8cee90a2b7601daf
uc-watkinwj <watkinwj@mail.uc.edu>
2019-09-27 20:36:04 -0400

    added comments to files

--- powershell/webpagelogin.ps1
+++ powershell/webpagelogin.ps1
@@ -1,7 +1,7 @@
-﻿$navOpenInBackgroundTab = 0x1000;
-$ie = new-object -com InternetExplorer.Application
-$ie.Navigate2("http://canopy.uc.edu/webapps/login/?action=relogin");
-$ie.Navigate2("https://www.twitch.tv", $navOpenInBackgroundTab);
+﻿$navOpenInBackgroundTab = 0x1000; #opens the window in the background
+$ie = new-object -com InternetExplorer.Application #calls internet explorer
+$ie.Navigate2("http://canopy.uc.edu/webapps/login/?action=relogin");#opens the site listed
+$ie.Navigate2("https://www.twitch.tv", $navOpenInBackgroundTab);#opens tabs behind the first
 $ie.Navigate2("https://www.reddit.com/r/legaladvice/ ", $navOpenInBackgroundTab);
 $ie.Navigate2("https://www.reddit.com/r/nfl/ ", $navOpenInBackgroundTab);
 $ie.Navigate2("https://www.reddit.com/r/fantasyfootball/ ", $navOpenInBackgroundTab);
