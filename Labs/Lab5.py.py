5848989067831ffa189a36b335c0a371170d5aef
uc-watkinwj <watkinwj@mail.uc.edu>
2019-09-28 16:12:49 -0400

    adding labs folder and lab5

---
+++ Labs/Lab5.py.py
@@ -0,0 +1,59 @@
+﻿import random #importing number game (pretty cool it is an import)
+
+guessesTaken = 0
+
+print('Hey, Welcome to the random number game!')
+
+
+
+
+number = random.randint(1, 50)
+
+print('The number is between 1 and 50.')
+
+
+
+while guessesTaken < 6:
+
+    print('Take a guess.') 
+
+    guess = input()
+
+    guess = int(guess)
+
+
+
+    guessesTaken = guessesTaken + 1
+
+
+
+    if guess < number:
+
+         print('Your guess is too low.') 
+
+
+
+    if guess > number:
+
+         print('Your guess is too high.')
+
+
+    if guess == number:
+
+         break
+
+
+
+if guess == number:
+
+     guessesTaken = str(guessesTaken)
+
+     print('Nice!, you guessed the number in ' + guessesTaken + ' guesses!')
+
+
+
+if guess != number:
+
+     number = str(number)
+
+     print('The random number was ' + number)
